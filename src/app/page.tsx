0cca2a21f3d369bfb52198ffb5e22b537d2adfce
Fabian Frahm <fabfrahm@gmail.com>
2026-02-15 19:18:43 +0100

    feat: add form to create tournaments
    Adds a form where users can create tournaments.
    
    Issue: #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
-export default function Home() {
+export default function TournamentForm() {
   return (
-    <div>Hello World</div>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">Create New Tournament</h1>
+      <form className="flex flex-col w-4/5 lg:w-1/4 space-y-1" action="">
+        <label htmlFor="name">Tournament Name</label>
+        <input
+          className="bg-white p-1 text-black border-2 border-black rounded"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Special Tournament 2026"
+        />
+
+        <label htmlFor="location">Location</label>
+        <input
+          className="bg-white p-1 text-black border-2 border-black rounded"
+          type="text"
+          name="location"
+          id="location"
+          placeholder="City, State, Country"
+        />
+
+        <label htmlFor="date">Date</label>
+        <input
+          className="bg-white p-1 text-black border-2 border-black rounded"
+          type="date"
+          name="date"
+          id="date"
+        />
+
+        <input
+          className="text-xl font-bold bg-blue-300 text-black rounded border-black border-2 p-2 mt-2"
+          type="submit"
+          value="Create Tournament"
+        />
+      </form>
+    </div>
   );
 }
