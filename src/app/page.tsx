9de30a46fba5151cfae7ceef599d593dc3293d5e
Fabian Frahm <fabfrahm@gmail.com>
2026-02-19 20:55:57 +0100

    feat: add simple processing logic for TournamentForm
    Issue: #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 export default function TournamentForm() {
+  async function createTournament(formData: FormData) {
+    "use server";
+
+    const tournament = {
+      name: formData.get("name"),
+      location: formData.get("location"),
+      date: formData.get("date"),
+    };
+
+    console.log(tournament);
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-center">Create New Tournament</h1>
-      <form className="flex flex-col w-4/5 lg:w-1/4 space-y-1" action="">
+      <form
+        className="flex flex-col w-4/5 lg:w-1/4 space-y-1"
+        action={createTournament}
+      >
         <label htmlFor="name">Tournament Name</label>
         <input
           className="bg-white p-1 text-black border-2 border-black rounded"
