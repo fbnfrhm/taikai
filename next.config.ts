1b17158df479cb72f6d5634a2d63c0464f952177
Fabian Frahm <fabfrahm@gmail.com>
2026-02-19 20:54:38 +0100

    chor: add allowedDevOrigins to next.config.ts
    allowedDevOrigins will be required by the next major update  of NextJS,
    therefore add them here, to be inline with framework requirements.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["localhost", "192.168.178.20"],
 };
 
 export default nextConfig;
